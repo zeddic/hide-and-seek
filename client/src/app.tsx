1d2570e47d303aec8c53a9886979fbf60390bc86
Scott Bailey <scott@zeddic.com>
2020-07-08 15:00:42 -0700

    Add base url to index.html

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState, SyntheticEvent, ChangeEvent} from 'react';
+import {ChatMessage} from 'lancer-shared/lib/messages';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './app.css';
 import {SocketService} from './socket_service';
-import {ChatMessage} from 'lancer-shared/lib/messages';
 
 const service = new SocketService();
 
