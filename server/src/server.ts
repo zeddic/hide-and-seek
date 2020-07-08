c4e8a0a43912d1053ad168f5f560795884482377
Scott Bailey <scott@zeddic.com>
2020-07-08 14:31:24 -0700

    Remove temporary 'yay'

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,6 @@ export class GameServer {
   private listen(): void {
     this.server.listen(this.port, () => {
       console.log('Running server on port %s', this.port);
-      console.log('Yay');
     });
 
     this.io.on(MessageType.CONNECT, (socket: Socket) => {
